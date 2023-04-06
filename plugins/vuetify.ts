e19d058fac47ee5ff56272788706b35c55519aaa
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-06 23:39:15 +0900

    Update theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,11 +1,20 @@
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import type { ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import {
   aliases,
   mdi
 } from 'vuetify/iconsets/mdi'
+
+const darkBlueTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#15202B',
+    surface: 'F7F9F9'
+  }
+}
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
@@ -19,7 +28,10 @@ export default defineNuxtPlugin((nuxtApp) => {
       }
     },
     theme: {
-      defaultTheme: 'dark'
+      defaultTheme: 'darkBlueTheme',
+      themes: {
+        darkBlueTheme
+      }
     }
   })
   nuxtApp.vueApp.use(vuetify)
