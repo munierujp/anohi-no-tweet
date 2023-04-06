123d155740e257b43f48b9634f2e5fafb75fc618
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-06 20:39:25 +0900

    Add Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: [
+      'vuetify'
+    ]
+  },
+  css: [
+    'vuetify/lib/styles/main.sass'
+  ],
   modules: [
     '@nuxtjs/eslint-module'
   ]
