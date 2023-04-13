3f8198e7217f754cfe7d0fea1180c98bf01b78e6
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-13 22:55:02 +0900

    Create createTwilogDateURL.test.ts

---
+++ utils/createTwilogDateURL.test.ts
@@ -0,0 +1,17 @@
+import {
+  describe,
+  expect,
+  it
+} from 'vitest'
+import { createTwilogDateURL } from './createTwilogDateURL'
+
+describe('createTwilogDateURL', () => {
+  it('returns the URL of Twisave for the given date.', () => {
+    const actual = createTwilogDateURL({
+      user: 'munieru_jp',
+      date: '2023-04-13'
+    })
+
+    expect(actual).toBe('https://twilog.org/munieru_jp/date-230413')
+  })
+})
