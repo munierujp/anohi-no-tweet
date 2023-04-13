38b54825d86b70b7bbf68d8274e8215af2d595a7
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-13 22:34:06 +0900

    Create createTwilogKeywordSearchURL.test.ts

---
+++ utils/createTwilogKeywordSearchURL.test.ts
@@ -0,0 +1,17 @@
+import {
+  describe,
+  expect,
+  it
+} from 'vitest'
+import { createTwilogKeywordSearchURL } from './createTwilogKeywordSearchURL'
+
+describe('createTwilogKeywordSearchURL', () => {
+  it('returns URL to search for keywords in Twilog.', () => {
+    const actual = createTwilogKeywordSearchURL({
+      user: 'munieru_jp',
+      keyword: 'hello'
+    })
+
+    expect(actual).toBe('https://twilog.org/munieru_jp/search?word=hello')
+  })
+})
