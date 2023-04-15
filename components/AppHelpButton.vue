4fed38837c677492cc20b553ea5d8172a1915e0c
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-15 21:44:26 +0900

    Create AppHelpButton.vue

---
+++ components/AppHelpButton.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+const show = ref(false)
+const openDialog = () => {
+  show.value = true
+}
+const closeDialog = () => {
+  show.value = false
+}
+</script>
+
+<template>
+  <v-btn
+    title="ヘルプ"
+    icon="mdi-help-circle"
+    @click="openDialog"
+  />
+  <v-dialog
+    v-model="show"
+    width="auto"
+  >
+    <v-card>
+      <v-card-text>指定した条件にマッチするツイートの検索結果、Twilog、ツイセーブなどを開きます。</v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="primary"
+          block
+          @click="closeDialog"
+        >
+          閉じる
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
