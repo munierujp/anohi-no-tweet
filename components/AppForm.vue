78d059566c33a940892761a8ed3ba2f5f3b71e99
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-15 18:20:54 +0900

    Update AppForm

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
 import {
+  parseISO,
   subDays,
   subMonths,
   subWeeks,
   subYears
 } from 'date-fns'
 
-const formStore = useFormStore()
 const now = new Date()
 const dateShortcuts: {
   text: string,
@@ -33,6 +33,44 @@ const dateShortcuts: {
     value: subYears(now, 1)
   }
 ]
+const formStore = useFormStore()
+const onClickSearchButton = () => {
+  const url = createTwitterSearchURL({
+    user: formStore.user,
+    date: parseISO(formStore.date),
+    keyword: formStore.keyword,
+    includesRetweets: formStore.includesRetweets
+  })
+  window.open(url)
+}
+const onClickTwilogDateButton = () => {
+  const url = createTwilogDateURL({
+    user: formStore.user,
+    date: parseISO(formStore.date)
+  })
+  window.open(url)
+}
+const onClickTwilogSearchButton = () => {
+  const url = createTwilogKeywordSearchURL({
+    user: formStore.user,
+    keyword: formStore.keyword
+  })
+  window.open(url)
+}
+const onClickTwisaveDateButton = () => {
+  const url = createTwisaveDateURL({
+    user: formStore.user,
+    date: parseISO(formStore.date)
+  })
+  window.open(url)
+}
+const onClickTwisaveSearchButton = () => {
+  const url = createTwisaveKeywordSearchURL({
+    user: formStore.user,
+    keyword: formStore.keyword
+  })
+  window.open(url)
+}
 </script>
 
 <template>
@@ -49,6 +87,7 @@ const dateShortcuts: {
       <el-date-picker
         v-model="formStore.date"
         type="date"
+        value-format="YYYY-MM-DD"
         :shortcuts="dateShortcuts"
         clearable
       />
@@ -64,6 +103,34 @@ const dateShortcuts: {
         v-model="formStore.includesRetweets"
         label="リツイートを含む"
       />
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        type="primary"
+        @click="onClickSearchButton"
+      >
+        検索
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="onClickTwilogDateButton">
+        Twilog（日付）
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="onClickTwilogSearchButton">
+        Twilog（検索）
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="onClickTwisaveDateButton">
+        ツイセーブ（日付）
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="onClickTwisaveSearchButton">
+        ツイセーブ（検索）
+      </el-button>
     </el-form-item>
   </el-form>
 </template>
