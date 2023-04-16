d00f2bf5f9f5261c856876ae1fcbb644ba0f6cb3
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-16 20:20:27 +0900

    Create AppIconLink.vue

---
+++ components/AppIconButton.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps<{
+  icon: string
+  title?: string
+}>()
+</script>
+
+<template>
+  <button
+    class="app-icon-button"
+    :title="title"
+  >
+    <Icon
+      :name="icon"
+      size="24px"
+    />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.app-icon-button {
+  border-radius: 50%;
+  padding: 8px;
+}
+</style>
