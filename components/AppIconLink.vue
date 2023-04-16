12fe14eb3cb7749862396291f0a58aa15795b3f9
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-16 20:29:48 +0900

    Create AppIconLink.vue

---
+++ components/AppIconLink.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+defineProps<{
+  href: string
+  icon: string
+  title?: string
+}>()
+</script>
+
+<template>
+  <a
+    class="app-icon-link"
+    :title="title"
+    :href="href"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <Icon
+      :name="icon"
+      size="24px"
+    />
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.app-icon-link {
+  border-radius: 50%;
+  color: unset;
+  padding: 8px;
+}
+</style>
