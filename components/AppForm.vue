9ae10a58c424ee9a3ec5ef45a63d5e417842c3cf
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-17 17:49:05 +0900

    Create AppFormTwilogSearch.vue

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -1,12 +1,5 @@
 <script lang="ts" setup>
 const form = useFormStore()
-const onClickTwilogSearchButton = () => {
-  const url = createTwilogKeywordSearchURL({
-    user: form.user,
-    keyword: form.keyword
-  })
-  window.open(url)
-}
 const onClickTwisaveSearchButton = () => {
   const url = createTwisaveKeywordSearchURL({
     user: form.user,
@@ -24,12 +17,7 @@ const onClickTwisaveSearchButton = () => {
     <AppFormIncludesRetweets />
     <AppFormSearch />
     <AppFormTwilogDate />
-    <!-- TODO: コンポーネント化 -->
-    <el-form-item>
-      <el-button @click="onClickTwilogSearchButton">
-        Twilog（検索）
-      </el-button>
-    </el-form-item>
+    <AppFormTwilogSearch />
     <AppFormTwisaveDate />
     <!-- TODO: コンポーネント化 -->
     <el-form-item>
