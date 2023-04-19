0d7cae50081a315104ed109bf5615e8e84ed2c96
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-20 01:44:26 +0900

    FIx key

--- composables/useConfigStore.ts
+++ composables/useConfigStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 
-export const useConfigStore = defineStore('form', {
+export const useConfigStore = defineStore('config', {
   state: () => ({
     theme: 'white'
   }),
