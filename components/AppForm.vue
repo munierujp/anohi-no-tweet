13febed83cf13747b13d2f03c078ab552fa3dc0d
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-22 23:28:05 +0900

    Remove AppFormSearch.vue

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -1,10 +1,33 @@
+<script lang="ts" setup>
+import { parseISO } from 'date-fns'
+
+const form = useFormStore()
+const openTwitter = () => {
+  const url = createTwitterSearchURL({
+    user: form.user,
+    date: parseISO(form.date),
+    keyword: form.keyword,
+    includesRetweets: form.includesRetweets
+  })
+  window.open(url)
+}
+</script>
+
 <template>
   <el-form label-position="top">
     <AppFormUser />
     <AppFormDate />
     <AppFormKeyword />
     <AppFormIncludesRetweets />
-    <AppFormSearch />
+    <el-form-item>
+      <el-button
+        type="primary"
+        :circle="false"
+        @click="openTwitter"
+      >
+        検索
+      </el-button>
+    </el-form-item>
     <AppFormTwilogDate />
     <AppFormTwilogSearch />
     <AppFormTwisaveDate />
