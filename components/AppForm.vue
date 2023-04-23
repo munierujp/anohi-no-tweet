47692927f413c682c79c625fd07b4826401b78eb
Munieru <20086673+munierujp@users.noreply.github.com>
2023-04-23 12:33:57 +0900

    Enterキーを押して検索できる機能を追加 (#82)
    * Remove AppFormSearch.vue
    
    * Add `@keydown.enter`
    
    * Remove AppFormUser.vue
    
    * Update help
    
    * Remove AppFormDate.vue
    
    * Remove AppFormKeyword.vue
    
    * Fix `@keydown.enter` event
    
    * Remove AppFormIncludesRetweets.vue
    
    * Remove AppFormTwilogDate.vue
    
    * Remove AppFormTwilogSearch.vue
    
    * Update style
    
    * Remove AppFormTwisaveDate.vue
    
    * Remove AppFormTwisaveSearch.vue
    
    * Update help
    
    * Update help

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -1,13 +1,161 @@
+<script lang="ts" setup>
+import {
+  parseISO,
+  subDays,
+  subYears
+} from 'date-fns'
+
+const form = useFormStore()
+const now = new Date()
+const setToToday = () => {
+  form.date = formatISODate(now)
+}
+const setToYesterday = () => {
+  form.date = formatISODate(subDays(now, 1))
+}
+const setToOneYearAgo = () => {
+  form.date = formatISODate(subYears(now, 1))
+}
+const openTwitter = () => {
+  const url = createTwitterSearchURL({
+    user: form.user,
+    date: parseISO(form.date),
+    keyword: form.keyword,
+    includesRetweets: form.includesRetweets
+  })
+  window.open(url)
+}
+const handleEnter = (event: KeyboardEvent | Event) => {
+  if (event instanceof KeyboardEvent && event.keyCode === 13) {
+    openTwitter()
+  }
+}
+const openTwilogDate = () => {
+  const url = createTwilogDateURL({
+    user: form.user,
+    date: parseISO(form.date)
+  })
+  window.open(url)
+}
+const openTwilogSearch = () => {
+  const url = createTwilogKeywordSearchURL({
+    user: form.user,
+    keyword: form.keyword
+  })
+  window.open(url)
+}
+const openTwisaveDate = () => {
+  const url = createTwisaveDateURL({
+    user: form.user,
+    date: parseISO(form.date)
+  })
+  window.open(url)
+}
+const openTwisaveSearch = () => {
+  const url = createTwisaveKeywordSearchURL({
+    user: form.user,
+    keyword: form.keyword
+  })
+  window.open(url)
+}
+</script>
+
 <template>
   <el-form label-position="top">
-    <AppFormUser />
-    <AppFormDate />
-    <AppFormKeyword />
-    <AppFormIncludesRetweets />
-    <AppFormSearch />
-    <AppFormTwilogDate />
-    <AppFormTwilogSearch />
-    <AppFormTwisaveDate />
-    <AppFormTwisaveSearch />
+    <el-form-item label="ユーザー">
+      <el-input
+        v-model="form.user"
+        placeholder="munieru_jp"
+        clearable
+        autofocus
+        @keydown.enter="handleEnter"
+      />
+    </el-form-item>
+    <el-form-item label="日付">
+      <el-date-picker
+        v-model="form.date"
+        type="date"
+        value-format="YYYY-MM-DD"
+        clearable
+        @keydown.enter="handleEnter"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        size="small"
+        :circle="false"
+        @click="setToToday"
+      >
+        今日
+      </el-button>
+      <el-button
+        size="small"
+        :circle="false"
+        @click="setToYesterday"
+      >
+        昨日
+      </el-button>
+      <el-button
+        size="small"
+        :circle="false"
+        @click="setToOneYearAgo"
+      >
+        1年前
+      </el-button>
+    </el-form-item>
+    <el-form-item label="キーワード">
+      <el-input
+        v-model="form.keyword"
+        clearable
+        @keydown.enter="handleEnter"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-checkbox
+        v-model="form.includesRetweets"
+        label="リツイートを含む"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        type="primary"
+        :circle="false"
+        @click="openTwitter"
+      >
+        検索
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        :circle="false"
+        @click="openTwilogDate"
+      >
+        Twilog（日付）
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        :circle="false"
+        @click="openTwilogSearch"
+      >
+        Twilog（検索）
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        :circle="false"
+        @click="openTwisaveDate"
+      >
+        ツイセーブ（日付）
+      </el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        :circle="false"
+        @click="openTwisaveSearch"
+      >
+        ツイセーブ（検索）
+      </el-button>
+    </el-form-item>
   </el-form>
 </template>
--- components/AppHelpButton.vue
+++ components/AppHelpButton.vue
@@ -18,7 +18,21 @@ const closeDialog = () => {
     v-model="showDialog"
     title="ヘルプ"
   >
-    <span>指定した条件にマッチするツイートの検索結果、Twilog、ツイセーブなどを開きます。</span>
+    <p>Twitterの検索結果を開くには、「検索」ボタンをクリック（タップ）するか、入力欄にフォーカスが当たった状態でEnterキーを押します。</p>
+    <p>
+      Twilogを開くには、「Twilog（日付）」ボタンまたは「Twilog（検索）」ボタンをクリック（タップ）します。なお、Twilogは2023年4月5日以降ツイートの取得を停止しています。<br>
+      <a
+        href="https://twitter.com/ropross/status/1643572802224783361"
+        target="_blank"
+      >https://twitter.com/ropross/status/1643572802224783361</a>
+    </p>
+    <p>
+      ツイセーブを開くには、「ツイセーブ（日付）」ボタンまたは「ツイセーブ（検索）」ボタンをクリック（タップ）します。なお、ツイセーブは2023年4月4日以降ツイートの取得を停止しています。<br>
+      <a
+        href="https://twisave.com/info/elonmusk"
+        target="_blank"
+      >https://twisave.com/info/elonmusk</a>
+    </p>
     <template #footer>
       <el-button
         :circle="false"
