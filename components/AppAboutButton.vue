643c6d49f1d08dedaf2043c1f007c5b5ef23d12c
Munieru <20086673+munierujp@users.noreply.github.com>
2025-04-27 01:21:11 +0900

    Create AppAboutButton.vue

---
+++ components/AppAboutButton.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const config = useAppConfig()
+const showDialog = ref(false)
+const openDialog = () => {
+  showDialog.value = true
+}
+const closeDialog = () => {
+  showDialog.value = false
+}
+</script>
+
+<template>
+  <AppIconButton
+    icon="mdi:information-circle"
+    title="About"
+    @click="openDialog"
+  />
+  <AppDialog
+    v-model="showDialog"
+    title="About"
+  >
+    <p>#あの日のツイートを表示するやつ は、Xのツイート（ポスト）を指定した条件で検索するためのウェブアプリです。ホーム画面に追加することで、ネイティブアプリのように使うことができます。</p>
+  </AppDialog>
+</template>
