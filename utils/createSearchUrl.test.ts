d739472e2eeb0e9600bbb604b64e7184768668cf
Munieru <20086673+munierujp@users.noreply.github.com>
2025-04-27 11:57:23 +0900

    Fix test

--- utils/createSearchUrl.test.ts
+++ utils/createSearchUrl.test.ts
@@ -7,58 +7,52 @@ import { createSearchUrl } from './createSearchUrl'
 
 describe('createSearchUrl', () => {
   describe('returns the URL', () => {
-    it.each<{
-      user: string
-      date: Date
-      keyword: string
-      includesRetweets: boolean
-      expected: string
-    }>([
-      {
-        user: '',
-        date: new Date(''),
-        keyword: '',
-        includesRetweets: false,
-        expected: 'https://x.com/search?f=live'
-      },
-      {
-        user: 'munieru_jp',
-        date: new Date(''),
-        keyword: '',
-        includesRetweets: false,
-        expected: 'https://x.com/search?f=live&q=from%3Amunieru_jp'
-      },
-      {
-        user: 'munieru_jp',
-        date: new Date('2023-04-15'),
-        keyword: '',
-        includesRetweets: false,
-        expected: 'https://x.com/search?f=live&q=from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-15_23%3A59%3A59_JST'
-      },
-      {
-        user: 'munieru_jp',
-        date: new Date('2023-04-15'),
-        keyword: 'hello',
-        includesRetweets: false,
-        expected: 'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-15_23%3A59%3A59_JST'
-      },
-      {
-        user: 'munieru_jp',
-        date: new Date('2023-04-15'),
-        keyword: 'hello',
-        includesRetweets: true,
-        expected: 'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-15_23%3A59%3A59_JST+include%3Anativeretweets'
-      }
-    ])('user=$user, date=$date, keyword=$keyword, includesRetweets=$includesRetweets', ({
+    it.each`
+      user            | startDate                 | endDate                   | keyword      | includesRetweets | expected
+      ${undefined}    | ${undefined}              | ${undefined}              | ${undefined} | ${false}         | ${'https://x.com/search?f=live'}
+      ${undefined}    | ${undefined}              | ${undefined}              | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=include%3Anativeretweets'}
+      ${undefined}    | ${undefined}              | ${undefined}              | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello'}
+      ${undefined}    | ${undefined}              | ${undefined}              | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+include%3Anativeretweets'}
+      ${undefined}    | ${undefined}              | ${new Date('2023-04-16')} | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${undefined}    | ${undefined}              | ${new Date('2023-04-16')} | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${undefined}    | ${undefined}              | ${new Date('2023-04-16')} | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${undefined}    | ${undefined}              | ${new Date('2023-04-16')} | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${undefined}              | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=since%3A2023-04-15_00%3A00%3A00_JST'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${undefined}              | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=since%3A2023-04-15_00%3A00%3A00_JST+include%3Anativeretweets'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${undefined}              | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+since%3A2023-04-15_00%3A00%3A00_JST'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${undefined}              | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+since%3A2023-04-15_00%3A00%3A00_JST+include%3Anativeretweets'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${undefined}    | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${undefined}              | ${undefined}              | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=from%3Amunieru_jp'}
+      ${'munieru_jp'} | ${undefined}              | ${undefined}              | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${undefined}              | ${undefined}              | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp'}
+      ${'munieru_jp'} | ${undefined}              | ${undefined}              | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${undefined}              | ${new Date('2023-04-16')} | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${'munieru_jp'} | ${undefined}              | ${new Date('2023-04-16')} | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${undefined}              | ${new Date('2023-04-16')} | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${'munieru_jp'} | ${undefined}              | ${new Date('2023-04-16')} | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${undefined}              | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${undefined}              | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${undefined}              | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${undefined}              | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${undefined} | ${false}         | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${undefined} | ${true}          | ${'https://x.com/search?f=live&q=from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${'hello'}   | ${false}         | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST'}
+      ${'munieru_jp'} | ${new Date('2023-04-15')} | ${new Date('2023-04-16')} | ${'hello'}   | ${true}          | ${'https://x.com/search?f=live&q=hello+from%3Amunieru_jp+since%3A2023-04-15_00%3A00%3A00_JST+until%3A2023-04-16_23%3A59%3A59_JST+include%3Anativeretweets'}
+    `('user=$user, startDate=$startDate, endDate=$endDate, keyword=$keyword, includesRetweets=$includesRetweets', ({
       user,
-      date,
+      startDate,
+      endDate,
       keyword,
       includesRetweets,
       expected
     }) => {
       const actual = createSearchUrl({
         user,
-        date,
+        startDate,
+        endDate,
         keyword,
         includesRetweets
       })
