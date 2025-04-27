c3723255566d4dc8dfbb79ecc52170e8009f7ac2
Munieru <20086673+munierujp@users.noreply.github.com>
2025-04-27 13:31:47 +0900

    sort

--- composables/useFormStore.ts
+++ composables/useFormStore.ts
@@ -2,11 +2,11 @@ import { defineStore } from 'pinia'
 
 export const useFormStore = defineStore('form', {
   state: () => ({
-    startDate: '',
     endDate: '',
-    syncDates: false,
     includesRetweets: true,
     keyword: '',
+    startDate: '',
+    syncDates: false,
     user: ''
   }),
   persist: true
